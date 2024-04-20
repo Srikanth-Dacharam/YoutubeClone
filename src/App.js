66226f8c93389f0cadf27c7e123bdfaa121966a2
Sam Srikanth <dacharamsrikanth130@gmail.com>
2024-04-20 22:23:46 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <div>Srikanth</div>
+      <Header />
+      <div>
+        <Sidebar />
+        <Routes>
+          <Route index element={<HomeScreen />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categoriesbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Categoriesbar() {
+  return <div>Categoriesbar</div>;
+}
