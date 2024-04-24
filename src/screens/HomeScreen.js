12c10f0b731db4d270c0b306eb69168407973c0a
Sam Srikanth <dacharamsrikanth130@gmail.com>
2024-04-24 20:56:38 +0530

    6

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function HomeScreen() {
-  return <div className="bg-red-200 rounded-md">HomeScreen</div>;
+  const [categories, setCategories] = useState([]);
+
+  const URL = "";
+  async function fetchCategories() {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setCategories(data);
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  return (
+    <div className="bg-red-200 rounded-md">
+      {categories.map((category) => {
+        <li>{}</li>;
+      })}
+    </div>
+  );
 }
